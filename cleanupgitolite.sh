6b12584d4e5cd937d9978e439cfcd9b843b185b9
b3yond <b3yond@riseup.net>
2016-09-01 20:05:58 +0200

    changed small stuff.

--- cleanupgitolite.sh
+++ cleanupgitolite.sh
@@ -2,6 +2,5 @@
 # removes gitolite. execute as gitolite user
 cd ~
 rm -rf gitolite .gitolite repositories foo .gitolite.rc projects.list .gitconfig bin/git*
-echo removed gitolite.
+echo removed gitolite. You may want to remove .ssh/authorized_keys.
 ls -la
-
