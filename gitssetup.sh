9f992c951d6b77fb580df8ce68d305da80814e1c
b3yond <b3yond@riseup.net>
2017-07-24 11:42:44 +0200

    wrote setup script

--- gitssetup.sh
+++ gitssetup.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
-cp ./gits /usr/bin/gits
-cp ./gits.py /usr/bin/gits.py
-chmod 775 /usr/bin/gits*
+#!/usr/bin/env python
+import os # unix commands?
+
+# ask credentials
+username = input("What's your git username?")
+useremail = input("What's your git user's email?")
+
+#configure git
+os.system("git config --global user.name \"" + username + "\"")
+os.system("git config --global user.email \"" + useremail + "\"")
+
+# make executable
+os.system("cp ./gits /usr/bin/gits")
+os.system("cp ./gits.py /usr/bin/gits.py")
+os.system("chmod 775 /usr/bin/gits*")
